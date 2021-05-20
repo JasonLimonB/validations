9e44455d7dbefc65181341236e5ca02faaf2a885
Monica Frias <friasmoni18@gmail.com>
2021-05-20 16:57:10 -0500

    Add attributes to the DTO

--- src/main/java/com/validations/dto/ValidationsDTO.java
+++ src/main/java/com/validations/dto/ValidationsDTO.java
@@ -1,6 +1,38 @@
 package com.validations.dto;
 
+
+import java.sql.Time;
+import java.time.LocalDate;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ValidationsDTO {
     // Put the attributes only
+	
+	@NotNull
+	private String loadActiontype;
+	@NotNull
+	private int loadCreateDOW;
+	@NotNull
+	private String destinationOrganizationCountryCode;
+	@NotNull
+	private String destinationOrganizationNumber;
+	@NotNull
+	private String destinationSortType; 
+	@NotNull
+	private String serviceTypeCodeMnemonicText;
+	@NotNull
+	private int loadPieceQuantity;
+	private Time loadLatestPossibleArrivalTime;
+	private int loadLatestArrivalDayOfWeek;
+	private String unloadLoadDestinationCountryCode;
+	@NotNull
+	private LocalDate recordEffectiveEndDate;
 
 }
--- src/main/java/com/validations/service/ValidationsService.java
+++ src/main/java/com/validations/service/ValidationsService.java
@@ -1,12 +1,27 @@
 package com.validations.service;
 
 import org.springframework.stereotype.Service;
+import com.validations.dto.ValidationsDTO;
+
 
 @Service
 public class ValidationsService {
 
     // put the validations (methods)
-    
+	
+	
+	public boolean validation_LoadActiontype() {
+		ValidationsDTO validDTO =  new ValidationsDTO();
+		validDTO.setLoadActiontype("");  
+		return (validDTO.getLoadActiontype().equals("ADD") || validDTO.getLoadActiontype().equals("UPDATE"))?  true : false;
+	}
+	
+	public boolean validation_LoadCreateDOW() {
+		ValidationsDTO validDTO =  new ValidationsDTO();
+		validDTO.setLoadCreateDOW(1);
+		return true;
+	}
 
+	
 
 }
